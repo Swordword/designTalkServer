692027f7a2163782fc85abb8e3bba7e902d8853d
littleCabbage <massivejohn123@gmail.com>
2020-01-14 00:15:52 +0800

    使用promise

--- controllers/home.js
+++ controllers/home.js
@@ -3,23 +3,38 @@ const charset = require('superagent-charset'),
   cherrio = require('cheerio')
 class homeCtl {
   async index(ctx) {
-    // ctx.body = '<h1>这是a a主页</h1>'
-    let item
-    item = []
-    // var url = 'https://www.dytt8.net/index0.html'
-    let data= await superagent
-      .get('http://127.0.0.1:5500/practice.html')
-      .charset('gbk')
-      .buffer(true)
-      .end((err, data) => {
-        if (err) {
-          throw err
-        }
-        const $ = cherrio.load(data.text)
-        let apple=$('.apple').text()
-        console.log("apple",apple);
-        ctx.body = 'apple'
-      })
+    const data = await new Promise(resolve => {
+      let item
+      item = []
+      var url = 'https://www.dytt8.net'
+      superagent
+        .get(url + '/index0.html')
+        .charset('gbk')
+        .buffer(true)
+        .end((err, data) => {
+          if (err) {
+            throw err
+          }
+          const $ = cherrio.load(data.text)
+          $('.bd3rl .co_area2').each(function(i, element) {
+            if (i > 1) {
+              return
+            }
+            let $element = $(element)
+            $element.find('tr').each((i1, ele) => {
+              let $ele = $(ele)
+              item.push({
+                href: $ele.find('a').attr('href'),
+                name: $ele.find('a').text()
+              })
+            })
+          })
+          ctx.statusCode = 200
+          resolve(item)
+        })
+    })
+    console.log('item', data)
+    ctx.body = data
   }
   getMovies() {}
 }
--- test/demo1.js
+++ test/demo1.js
@@ -1,51 +1,52 @@
-const Koa = require('koa'),
-  Router = require('koa-router'),
-  cheerio = require('cheerio'),
-  charset = require('superagent-charset'),
-  superagent = charset(require('superagent')),
-  app = new Koa(),
-  router = new Router();
-let arr;
+const Koa = require('koa')
+// SuperAgent是一个轻量级、灵活的、易读的、低学习曲线的客户端请求代理模块，使用在NodeJS环境中。
+const request = require('superagent')
+// cheerio是nodejs的抓取页面模块，为服务器特别定制的jQuery核心实现。适合各种Web爬虫程序。
+const cheerio = require('cheerio')
+// 引入 path 模块中的 join 用于拼接正确的路径
+const { join } = require('path')
+const app = new Koa() // 这里要注意我们引入的koa是一个构造函数, 需要去实例它
+// Koa 应用程序是一个包含一组中间件函数的对象
+app.use(async (ctx, next) => {
+  // ctx 这是 Context, 具体用法大家去看文档哈,很详细
+  //async next() 当一个中间件调用 next() 则该函数暂停并将控制传递给定义的下一个中间件。
+  const arr = [] // 创建一个数组存储我们要抓取页面的数据
+  const URL = 'http://www.wtown.com.cn/' // 保存我们要抓取页面的地址
 
-router.get('/', (ctx, next) => {
-  url = 'http://shop.bytravel.cn/produce/index226.html'; //target地址
-  superagent.get(url)
-    .charset('gbk')  // 当前页面编码格式
-    .buffer(true)
-    .end((err, data) => { //页面获取到的数据
-      if (err) {
-        // return next(err); 
-        console.log('页面不存在', err)
-      }
-      let html = data.text,
-        $ = cheerio.load(html, {
-          decodeEntities: false,
-          ignoreWhitespace: false,
-          xmlMode: false,
-          lowerCaseTags: false
-        }), //用cheerio解析页面数据
-      obj = {};
-      arr = [];
-      // cheerio的使用类似jquery的操作
-      $("table tbody").each((index, element) => {
-        let $element = $(element);
-        $element.find('#tctitle').next().find('a').addClass('link').attr('class', 'link').text('')
-        arr.push({
-          'title': $element.find('a.blue14b').text(),
-          'image': $element.find('#bright img').attr('src'),
-          'summary': $element.find('#tctitle').next().text(),
-          'is_cgiia': $element.find('#tctitle font').attr('color') === 'green' ? 1 : 0
+  // 重点注意的是, request请求是一个异步,因此要把他转为一个Promise对象,等待获取完数据在返回出去
+  const data =await new Promise(resolve => {
+    request
+      // 这里我抓取的是一个景区官网的票务信息
+      .get('http://www.wtown.com.cn/index.php/Reservation/Ticket/index.html')
+      .end((err, res) => {
+        const data = res.text // 我需要的信息在text中
+        const $ = cheerio.load(data) // 通过 cheerio 模块转为DOM结构
+        // 分析页面DOM结构抓取相应的数据
+        $('.box ul li').each((i, v) => {
+          // 因为获取的不止一个,所以要each遍历保存数据
+          const $v = $(v)
+          const obj = {
+            // join 用于拼接正确的路径
+            img: join(URL, $v.find('.ticket-left img').prop('src')),
+            // // join 用于拼接正确的路径
+            src: join(URL, $v.find('.ticket-right .fs24 a').prop('href')),
+            text: $v
+              .find('.ticket-right .txt')
+              .text()
+              .trim(),
+            price: $v
+              .find('.ticket-right p .fs24')
+              .text()
+              .trim()
+          }
+          arr.push(obj)
         })
+        console.log('arr', arr)
+        resolve(arr) // 当数据都存到 arr 中以后 resolve 出去
       })
-    })
-  ctx.body = arr
-  // console.log(arr)
+  })
+  console.log('data', data)
+
+  ctx.body = data // 将我们抓取到的数据返回到页面中显示
 })
-
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-app.listen(3010, () => {
-  console.log('[服务已开启,访问地址为：] http://127.0.0.1:3010/');
-});
+app.listen(3010) // 这里我们监听3000端口
