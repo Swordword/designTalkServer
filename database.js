abd8a89e6b4b310b2bce70847539b04617971fc7
John Massive <massivejohn123@gmail.com>
2020-01-19 18:58:20 +0800

    feat 开始mongodb

---
+++ database.js
@@ -0,0 +1,17 @@
+let mongo = require('mongoose')
+const server = '127.0.0.1:27017'
+const database = 'fcc-Mail'
+class Database{
+    constructor() {
+        this._connect()
+    }
+    _connect(){
+        mongo.connect(`mongodb://${server}/${database}`).then((result) => {
+            console.log('连接成功',result)
+        }).catch((err) => {
+            throw err
+        });
+    }
+}
+
+module.exports = new Database()
