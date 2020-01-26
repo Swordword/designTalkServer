0b87b0d370a00a27a36d8858b3344589776482a9
littleCabbage <massivejohn123@gmail.com>
2020-01-26 23:06:01 +0800

    feat 在遍历中下载图片

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,58 +1,53 @@
 const charset = require('superagent-charset'),
-  superagent = charset(require('superagent')),
-  cherrio = require('cheerio')
+	superagent = charset(require('superagent')),
+	cherrio = require('cheerio')
+const { download } = require('./download')
 
 module.exports = function () {
-  return new Promise(resolve => {
-    let result = []
-    var url = 'https://dribbble.com'
-    superagent
-      .get(url)
-      .charset()
-      .buffer(true)
-      .end((err, data) => {
-        if (err) {
-          throw err
-        }
-        // 获取单个页面的图片
-        getSingleImg()
-      })
-    console.log('result11', result)
-
-    resolve(result)
-  })
+	var url = 'https://dribbble.com'
+	superagent
+		.get(url)
+		.charset()
+		.buffer(true)
+		.end((err, data) => {
+			if (err) {
+				throw err
+			}
+			const $ = cherrio.load(data.text)
+			let item = []
+			$('.dribbble-img').each(function (i, element) {
+				if (i > 30) {
+					return
+				}
+				let $element = $(element)
+				let linkUrl = $element.find('.dribbble-link').attr('href')
+				superagent
+					.get(url + linkUrl)
+					.charset()
+					.buffer(true)
+					.end(async (err, data2) => {
+						if (err) {
+							throw err
+						}
+						const $1 = cherrio.load(data2.text)
+						let href = $1('.media-content img').attr('data-src')
+						item.push({
+							name: $1('.shot-title').text(),
+							href: href
+						})
+						if (href) {
+							await download(href)
+						}
+						console.log('itemin', item)
+					})
+			})
+		})
 }
 
-getSingleImg(){
+// function getSingleImg(data) {
 
-  const $ = cherrio.load(data.text)
-  let item = []
-  $('.dribbble-img').each(function (i, element) {
-    if (i > 3) {
-      return
-    }
-    let $element = $(element)
-    let linkUrl = $element.find('.dribbble-link').attr('href')
-    superagent
-      .get(url + linkUrl)
-      .charset()
-      .buffer(true)
-      .end((err, data2) => {
-        if (err) {
-          throw err
-        }
-        const $1 = cherrio.load(data2.text)
-        console.log('AAAA', $1('.media-content img'))
+// 	// const url = 'https://dribbble.com'
 
-        item.push({
-          name: $1('.shot-title').text(),
-          href: $1('.media-content img').attr('data-src')
-        })
-        console.log('itemin', item)
-      })
-  })
-  console.log('item11', item)
-
-}
+// }
 // 判断url start with http
-function checkUrl(url) { }
+// function checkUrl(url) { }
